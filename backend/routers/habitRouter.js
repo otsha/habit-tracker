b7f746c05de5fc8e4256b8baaa00dfbc02000213
otsha <32258613+otsha@users.noreply.github.com>
2019-04-10 11:49:02 +0300

    BACKEND: Implement posting and deleting habits to a session-sensitive list

--- backend/routers/habitRouter.js
+++ backend/routers/habitRouter.js
@@ -2,23 +2,58 @@ const habitRouter = require('express').Router()
 
 const habitA = {
   name: 'Habit #A',
-  important: false
+  important: false,
+  id: 1
 }
 
 const habitB = {
   name: 'Habit #B',
-  important: true
+  important: true,
+  id: 2
 }
 
 const habitC = {
   name: 'Habit #C',
-  important: false
+  important: false,
+  id: 3
 }
 
-const habits = [habitA, habitB, habitC]
+let habits = [habitA, habitB, habitC]
 
 habitRouter.get('/', (req, res) => {
   res.json(habits).status(200)
 })
+
+habitRouter.get('/:id', (req, res) => {
+  const habit = habits.find(h => h.id === Number(req.params.id))
+  if (habit) {
+    res.json(habit).status(200)
+  } else {
+    res.status(404).end()
+  }
+})
+
+habitRouter.post('/', (req, res) => {
+  const habitObject = req.body
+
+  let id = Math.floor(Math.random() * 10000)
+  while (habits.find(h => h.id === id)) {
+    id = Math.floor(Math.random() * 10000)
+  }
+
+  const habitToAdd = {
+    ...habitObject,
+    id: id
+  }
+
+  habits.push(habitToAdd)
+  res.json(habitToAdd).status(201)
+})
+
+habitRouter.delete('/:id', (req, res) => {
+  habits = habits.filter(h => h.id !== Number(req.params.id))
+  res.status(204).end()
+})
+
 
 module.exports = habitRouter
