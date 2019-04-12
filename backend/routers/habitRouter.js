fe1d1f88f6b7e9ad81a1392047c585259792889d
otsha <32258613+otsha@users.noreply.github.com>
2019-04-12 11:39:54 +0300

    BACKEND: Update habit operations to use Atlas via Mongoose

--- backend/routers/habitRouter.js
+++ backend/routers/habitRouter.js
@@ -1,33 +1,13 @@
 const habitRouter = require('express').Router()
 const Habit = require('../models/habit')
 
-const habitA = {
-  name: 'Habit #A',
-  important: false,
-  id: 1
-}
-
-const habitB = {
-  name: 'Habit #B',
-  important: true,
-  id: 2
-}
-
-const habitC = {
-  name: 'Habit #C',
-  important: false,
-  id: 3
-}
-
-let habitList = [habitA, habitB, habitC]
-
 habitRouter.get('/', async (req, res) => {
   const habits = await Habit.find({})
   res.json(habits).status(200)
 })
 
-habitRouter.get('/:id', (req, res) => {
-  const habit = habits.find(h => h.id === Number(req.params.id))
+habitRouter.get('/:id', async (req, res) => {
+  const habit = await Habit.findById(req.params.id)
   if (habit) {
     res.json(habit).status(200)
   } else {
@@ -43,19 +23,17 @@ habitRouter.post('/', async (req, res) => {
   res.json(result).status(201)
 })
 
-habitRouter.delete('/:id', (req, res) => {
-  habits = habits.filter(h => h.id !== Number(req.params.id))
+habitRouter.delete('/:id', async (req, res) => {
+  const habitToDelete = await Habit.findByIdAndDelete(req.params.id)
   res.status(204).end()
 })
 
-habitRouter.put('/:id', (req, res) => {
-  const habit = habits.find(h => h.id === Number(req.params.id))
-  const updatedHabit = req.body
-  if (habit) {
-    habits = habits.map(h => h.id === habit.id ? updatedHabit : h)
-    res.json(updatedHabit).status(200).end()
-  } else {
-    res.status(400).end()
+habitRouter.put('/:id', async (req, res) => {
+  try {
+    await Habit.findByIdAndUpdate(req.params.id, req.body)
+    res.status(200).end()
+  } catch (exception) {
+    res.json(exception).status(400).end()
   }
 })
 
