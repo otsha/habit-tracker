f015f35a397cbee392c08c31fe505398b1924cec
otsha <32258613+otsha@users.noreply.github.com>
2019-04-30 11:35:15 +0300

    FRONTEND: Fetched dates are now user-specific

--- habit-tracker/src/services/dateService.js
+++ habit-tracker/src/services/dateService.js
@@ -1,20 +1,25 @@
 import axios from 'axios'
+import authService from './authService'
+
 const url = 'http://localhost:3001/api/dates'
 
 const getAll = async () => {
-  const res = await axios.get(url)
+  const config = { headers: { Authorization: authService.getToken() } }
+  const res = await axios.get(url, config)
   console.log('Server returned these dates:', res.data)
   return res.data
 }
 
 const addNew = async (dateObject) => {
-  const res = await axios.post(url, dateObject)
+  const config = { headers: { Authorization: authService.getToken() } }
+  const res = await axios.post(url, dateObject, config)
   console.log('Server returned added date: ', res.data)
   return res.data
 }
 
 const update = async (id, habit) => {
-  const res = await axios.put(`${url}/${id}`, habit)
+  const config = { headers: { Authorization: authService.getToken() } }
+  const res = await axios.put(`${url}/${id}`, habit, config)
   console.log('Server returned updated date:', res.data)
   return res.data
 }
